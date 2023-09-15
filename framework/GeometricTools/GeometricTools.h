8ab0d4f1e603c859955d250379a966f1cdd0c5cd
Evan <evanfr@stud.ntnu.no>
2023-09-15 11:37:07 +0200

    Add GeometricTools.h

--- framework/GeometricTools/GeometricTools.h
+++ framework/GeometricTools/GeometricTools.h
@@ -1,4 +1,19 @@
 #ifndef PROG2002_GEOMETRICTOOLS_H
 #define PROG2002_GEOMETRICTOOLS_H
 
+#include <array>
+
+namespace GeometricTools {
+    constexpr std::array<float, 3*2> UnitTriangle2D = {-0.5f, -0.5f, 0.5f, -0.5f, 0.0f, 0.5f}; // [2,3]
+
+    constexpr std::array<float, 3*4> UnitSquare2D = {-0.5f, -0.5f, 0.5f, -0.5f, 0.5f,  0.5f, -0.5f,  0.5f}; // [2,4]
+
+    constexpr std::array<float, 3*4> UnitGridGeometry2D = {-0.5f, -0.5f, 0.5f, -0.5f, 0.5f,  0.5f, -0.5f,  0.5f}; // [2,4]
+
+    constexpr std::array<unsigned int, 3*2> UnitGridTopologyTriangles = {0, 1, 2, 0, 2, 3}; // [2,3]
+
+
+}
+
+
 #endif //PROG2002_GEOMETRICTOOLS_H
