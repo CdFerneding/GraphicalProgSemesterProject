6fff161aee865c515c816d18f28d5dc21d64c415
CdFerneding <bencf@stud.ntnu.no>
2023-09-20 16:13:34 +0200

    lab1: drawing square finished.

--- labs/lab1/main.cpp
+++ labs/lab1/main.cpp
@@ -6,9 +6,9 @@
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
 
-int main(int argc, char** argv)
+int draw_triangle()
 {
-    // Initialize GLFW
+    // Initialize GLFW (Graphics Lbrary Framework)
     if (glfwInit() == GLFW_FALSE) {
         std::cerr << "Failed to initialize GLFW" << std::endl;
         return EXIT_FAILURE;
@@ -37,7 +37,7 @@ int main(int argc, char** argv)
     }
 
     // Vertex data for a triangle
-    float triangle[3 * 2] = {
+    float triangle[3 * 2] = { // 3 vertices x 2 coordinate components
         -0.5f, -0.5f,
         0.5f, -0.5f,
         0.0f,  0.5f
@@ -52,9 +52,11 @@ int main(int argc, char** argv)
     GLuint vertexBufferId;
     glGenBuffers(1, &vertexBufferId);
     glBindBuffer(GL_ARRAY_BUFFER, vertexBufferId);
+
+    //populate the vertex buffer
     glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW);
 
-    // Define the vertex attribute layout
+    // Define the vertex attribute layout of the bound buffer
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, nullptr);
     glEnableVertexAttribArray(0);
 
@@ -131,4 +133,149 @@ int main(int argc, char** argv)
     // Clean up GLFW and exit
     glfwTerminate();
     return EXIT_SUCCESS;
+}
+
+int draw_square() {
+
+    // Initialize GLFW (Graphics Lbrary Framework)
+    if (glfwInit() == GLFW_FALSE) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // GLFW window hints
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    // Create a GLFW window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Triangle", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+
+    // Load OpenGL functions with GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+
+    // triangles to draw a square
+    float square_part1[6 * 2] = {
+        0.5f, 0.5f,
+        -0.5f, 0.5f,
+        -0.5f, -0.5f,
+
+        0.5f, 0.5f,
+        -0.5f, -0.5f,
+        0.5f, -0.5f
+    };
+
+    // Create a vertex array object (VAO)
+    GLuint vertexArrayId;
+    glGenVertexArrays(2, &vertexArrayId);
+    glBindVertexArray(vertexArrayId);
+
+    // Create a vertex buffer object (VBO)
+    GLuint vertexBufferId;
+    glGenBuffers(2, &vertexBufferId);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferId);
+
+    //populate the vertex buffer
+    glBufferData(GL_ARRAY_BUFFER, sizeof(square_part1), square_part1, GL_STATIC_DRAW);
+
+    // Define the vertex attribute layout of the bound buffer
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, nullptr);
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+
+    // Vertex and fragment shader source code
+    const std::string vertexShaderSrc = R"(
+    #version 430 core
+    layout(location = 0) in vec2 position;
+    void main()
+    {
+        gl_Position = vec4(position, 0.0, 1.0);
+    }
+)";
+
+    const std::string fragmentShaderSrc = R"(
+    #version 430 core
+    out vec4 color;
+    void main()
+    {
+        color = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+)";
+
+    // Compile the vertex shader
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    const GLchar* vss = vertexShaderSrc.c_str();
+    glShaderSource(vertexShader, 1, &vss, nullptr);
+    glCompileShader(vertexShader);
+
+    // Compile the fragment shader
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    const GLchar* fss = fragmentShaderSrc.c_str();
+    glShaderSource(fragmentShader, 1, &fss, nullptr);
+    glCompileShader(fragmentShader);
+
+    // Create a shader program
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // Check shader program linking status
+    GLint success;
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        char infoLog[512];
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cerr << "Shader program linking failed: " << infoLog << std::endl;
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+
+    // Clean up shader objects
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // Use the shader program
+    glUseProgram(shaderProgram);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // Clear the screen
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw the square by drawing two triangles
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+
+        // Poll for and process events
+        glfwPollEvents();
+    }
+
+    // Clean up GLFW and exit
+    glfwTerminate();
+    return EXIT_SUCCESS;
+}
+
+
+int main(int argc, char** argv)
+{
+    if (0) { 
+        draw_triangle(); 
+    }
+    if (1) {
+        draw_square();
+    }
 }
