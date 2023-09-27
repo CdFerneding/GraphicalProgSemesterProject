9d62a177000cf0bab294c2309c09e1efc7ae32db
Evan <evanfr@stud.ntnu.no>
2023-09-27 16:53:04 +0200

    Fix binding in the constructor instead of the binding function in IndexBuffer

--- framework/Rendering/IndexBuffer.cpp
+++ framework/Rendering/IndexBuffer.cpp
@@ -3,7 +3,6 @@
 IndexBuffer::IndexBuffer(GLuint *indices, GLsizei count) {
     this->Count = count;
     glGenBuffers(count, &(this->IndexBufferID));
-    glBindBuffer(GL_ARRAY_BUFFER, this->IndexBufferID);
     glBufferData(GL_ARRAY_BUFFER, sizeof(unsigned int) * count, indices, GL_STATIC_DRAW);
 }
 
