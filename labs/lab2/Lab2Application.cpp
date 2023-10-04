e850450836de515a34ac1c72da17aadd8b130e66
CdFerneding <bencf@stud.ntnu.no>
2023-10-04 17:01:38 +0200

    Lab 2 update. Finished Lab 2 with Shader Module. Solved conflicts with updated Vertex Array,Buffer and index Buffer.

--- labs/lab2/Lab2Application.cpp
+++ labs/lab2/Lab2Application.cpp
@@ -4,6 +4,7 @@
 #include "../../framework/Rendering/IndexBuffer.h"
 #include "./../Rendering/VertexBuffer.h"
 #include "VertextArray.h"
+#include "../../framework/Rendering/Shader.h"
 
 Lab2Application::Lab2Application(const std::string &name, const std::string &version, 
     unsigned int width, unsigned int height): GLFWApplication(name, version, width, height) {
@@ -14,50 +15,6 @@ Lab2Application::~Lab2Application() {
 
 }
 
-//
-// Function to implement programmed shaders:
-//
-GLuint CompileShader(const std::string& vertexShaderSrc,
-                     const std::string& fragmentShaderSrc)
-{
-    // Convert shader source code from std::string to raw char pointer.
-    auto vertexSrc = vertexShaderSrc.c_str();
-    auto fragmentSrc = fragmentShaderSrc.c_str();
-
-    // Compile vertex shader
-    auto vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexSrc, nullptr);
-    glCompileShader(vertexShader);
-
-    // Compile fragment shader
-    auto fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentSrc, nullptr);
-    glCompileShader(fragmentShader);
-
-    // Link shaders into a program
-    auto shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
-    // Check shader program linking status
-    GLint success;
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        char infoLog[512];
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cerr << "Shader program linking failed: " << infoLog << std::endl;
-        glfwTerminate();
-        return EXIT_FAILURE;
-    }
-
-    // Clean up shader objects as they're no longer needed after linking
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-    glUseProgram(shaderProgram);
-
-    return shaderProgram;
-}
 
 unsigned Lab2Application::Run() const {
     auto triangle = GeometricTools::UnitSquare2D;
@@ -88,6 +45,7 @@ unsigned Lab2Application::Run() const {
     //glGenVertexArrays(1, &vertexArrayId);
     //glBindVertexArray(vertexArrayId);
 
+
     //
     // index module
     //
@@ -97,23 +55,25 @@ unsigned Lab2Application::Run() const {
     // Define the vertex attribute layout of the bound buffer
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, nullptr);
     glEnableVertexAttribArray(0);
-    
 
-    GLuint squareShaderProgram = CompileShader(vertexShaderSrc, fragmentShaderSrc);
+
+    //
+    // Shader module
+    //
+    Shader shader(vertexShaderSrc, fragmentShaderSrc);
+
 
     while (!glfwWindowShouldClose(window))
     {
-
-        //preparation
+        
+        //preparation of Window and Shader
         glClear(GL_COLOR_BUFFER_BIT);
-
-
-        glUseProgram(squareShaderProgram);
+        shader.Bind();
         glDrawElements(
-                GL_TRIANGLES,      // mode
-                6,    // count
-                GL_UNSIGNED_INT,   // type
-                (void*)0           // element array buffer offset
+                GL_TRIANGLES,       // mode
+                6,                  // count
+                GL_UNSIGNED_INT,    // type
+                (void*)0            // element array buffer offset
         );
 
         //buffer and drawing
@@ -124,6 +84,11 @@ unsigned Lab2Application::Run() const {
         glfwPollEvents();
     }
 
+    // Cleanup of Shader and Buffers
+    shader.Unbind();
+    shader.~Shader();
+    vertexbuffer.Unbind();
+    vertexbuffer.~VertexBuffer();
     vertexArray->Unbind();
     vertexArray->~VertexArray();
 
