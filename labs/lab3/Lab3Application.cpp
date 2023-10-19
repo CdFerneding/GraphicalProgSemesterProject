826a2586673a78262d7754aba6498c1f56a2c278
CdFerneding <bencf@stud.ntnu.no>
2023-10-19 14:00:09 +0200

    camera floor finished (issue 11)

--- labs/lab3/Lab3Application.cpp
+++ labs/lab3/Lab3Application.cpp
@@ -9,53 +9,119 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-//#include "../../framework/ErrorHandling/ErrorHandling.h"
+Lab3Application* Lab3Application::current_application = nullptr;
 
-Lab3Application::Lab3Application(const std::string &name, const std::string &version,
-                                 unsigned int width, unsigned int height): GLFWApplication(name, version, width, height) {
-
+Lab3Application::Lab3Application(const std::string& name, const std::string& version,
+    unsigned int width, unsigned int height) : GLFWApplication(name, version, width, height) {
+    currentXSelected = 0;
+    currentYSelected = 0;
 }
 
 Lab3Application::~Lab3Application() {
 
 }
+void Lab3Application::key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
 
+    if (action == GLFW_PRESS) {
+        switch (key) {
+        case GLFW_KEY_UP:
+            getLab3Application()->move(UP);
+            break;
+        case GLFW_KEY_DOWN:
+            getLab3Application()->move(DOWN);
+            break;
+        case GLFW_KEY_LEFT:
+            getLab3Application()->move(LEFT);
+            break;
+        case GLFW_KEY_RIGHT:
+            getLab3Application()->move(RIGHT);
+            break;
+        case GLFW_KEY_ESCAPE:
+            glfwSetWindowShouldClose(window, GLFW_TRUE);
+            break;
+        default:
+            break;
+        }
+    }
+}
+void Lab3Application::move(Direction direction) {
+    hasMoved = true;
+    switch (direction) {
+    case UP:
+        currentYSelected = (currentYSelected + 1) % numberOfSquare;
+        break;
+    case DOWN:
+        currentYSelected = (currentYSelected - 1) % numberOfSquare;
+        break;
+    case LEFT:
+        currentXSelected = (currentXSelected + 1) % numberOfSquare;
+        break;
+    case RIGHT:
+        currentXSelected = (currentXSelected - 1) % numberOfSquare;
+        break;
+    default:
+        hasMoved = false;
+        break;
+    }
+    //std::cout << "Current X: " << currentXSelected << std::endl;
+    //std::cout << "Current Y: " << currentYSelected << std::endl;
+}
+
+std::vector<float> Lab3Application::createSelectionSquare() const {
+
+    std::vector<float> selectionSquare = {
+            1 - (2.0f / (float)numberOfSquare) * (float(currentXSelected)), -1 + (2.0f / (float)numberOfSquare) * (float(currentYSelected)), 0.0, 0, 1, 0, 1,
+            1 - (2.0f / (float)numberOfSquare) * (float(currentXSelected)), -1 + (2.0f / (float)numberOfSquare) * (float(currentYSelected + 1)), 0.0, 0, 1, 0, 1,
+            1 - (2.0f / (float)numberOfSquare) * (float(currentXSelected + 1)), -1 + (2.0f / (float)numberOfSquare) * (float(currentYSelected + 1)),  0.0, 0, 1, 0, 1,
+            1 - (2.0f / (float)numberOfSquare) * (float(currentXSelected + 1)), -1 + (2.0f / (float)numberOfSquare) * (float(currentYSelected)), 0.0, 0, 1, 0, 1
+    };
+
+    //print the selection square
+    /*for (int i = 0; i < selectionSquare.size(); i+=7) {
+        std::cout << selectionSquare[i] << ", " << selectionSquare[i+1] << ", " << selectionSquare[i+2] << std::endl;
+    }*/
+
+    return selectionSquare;
+}
 
 unsigned Lab3Application::Run() {
-    unsigned numberOfSquare = 8;
-    auto triangle = GeometricTools::UnitGrid2D(numberOfSquare);
+    current_application = this;
+    //auto triangle = GeometricTools::UnitGrid2D(numberOfSquare);
+    auto triangle = GeometricTools::UnitGrid2DWithColor(numberOfSquare); //code with the color not in the shader
 
+    auto selectionSquare = createSelectionSquare();
+
+    triangle.insert(triangle.end(), selectionSquare.begin(), selectionSquare.end());
+
+    //std::cout << triangle.size() << " " << triangle.size() / 7 << std::endl;
     auto vertexArray = std::make_shared<VertexArray>();
     auto indices = GeometricTools::UnitGrid2DTopology(numberOfSquare);
 
+    auto indicesSelectionSquare = GeometricTools::TopologySquare2D;
+
+    for (unsigned i : indicesSelectionSquare) {
+        indices.push_back((numberOfSquare + 1) * (numberOfSquare + 1) * 2 + i);
+    }
+
     auto indexBuffer = std::make_shared<IndexBuffer>(indices.data(), sizeof(unsigned int) * indices.size());
-    auto gridBufferLayout = BufferLayout({
-        { ShaderDataType::Float3, "position" },
-        { ShaderDataType::Float4, "color" }
-    });
+    auto gridBufferLayout = std::make_shared<BufferLayout>(BufferLayout({
+        {ShaderDataType::Float3, "position", false}
+        ,{ShaderDataType::Float4, "color", false} // When we use the color in the vertexBuffer
+        }));
+
     auto vertexBuffer = std::make_shared<VertexBuffer>(triangle.data(), sizeof(float) * triangle.size());
 
-    vertexBuffer->SetLayout(gridBufferLayout);
+
+    //auto vertexBufferColor = std::make_shared<VertexBuffer>(color.data(), sizeof(float) * color.size());
+    //vertexBufferColor->SetLayout(*gridBufferLayout2);
+
+
+    vertexBuffer->SetLayout(*gridBufferLayout);
+    //vertexArray->AddVertexBuffer(vertexBufferColor);
     vertexArray->AddVertexBuffer(vertexBuffer);
     vertexArray->SetIndexBuffer(indexBuffer);
 
     vertexArray->Bind();
-
-    //glGenVertexArrays(1, &vertexArrayId);
-    //glBindVertexArray(vertexArrayId);
-
-
-    //
-    // index module
-    //
-    //IndexBuffer indexBuffer(indices, 6);
-    //indexBuffer.Bind();
-
-    // Define the vertex attribute layout of the bound buffer
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, nullptr);
-    glEnableVertexAttribArray(0);
-
 
     //
     // Shader module
@@ -70,7 +136,7 @@ unsigned Lab3Application::Run() {
     glm::mat4 projectionMatrix = glm::perspective(glm::radians(45.0f), 1.0f, 1.0f, -10.0f);
 
     // view transformation Matrix: position and orientation of the matrix
-    // --> position of the camera ("eye"/position of the camera, Position where the camera is looking at, Normalized up vektor)
+    // --> position of the camera ("eye"/position of the camera, Position where the camera is looking at, Normalized up vector)
     glm::mat4 viewMatrix = glm::lookAt(glm::vec3(0, 0, 5), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
 
     // model transformation: scale, rotate, translate (model = scale*rotate*translate)
@@ -87,23 +153,32 @@ unsigned Lab3Application::Run() {
 
     shader->UploadUniformMatrix4fv("u_Model", chessboardModelMatrix);
     shader->UploadUniformMatrix4fv("u_View", viewMatrix);
-    shader->UploadUniformMatrix4fv("u_Projection", projectionMatrix); 
+    shader->UploadUniformMatrix4fv("u_Projection", projectionMatrix);
 
     glm::vec4 color = glm::vec4(0.0, 0.0, 0.0, 1.0);
     shader->UploadUniformFloat4("u_Color", color);
+
+    glfwSetKeyCallback(window, Lab3Application::key_callback);
+
 
     while (!glfwWindowShouldClose(window))
     {
 
         //preparation of Window and Shader
-        glClear(GL_COLOR_BUFFER_BIT);
         glClearColor(0.663f, 0.663f, 0.663f, 1.0f); // Set clear color to a shade of gray
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        // Update the vertex buffer with the new data for the selection square using the currentXSelected and currentYSelected
+        if (hasMoved) {
+            vertexBuffer->BufferSubData(sizeof(float) * 7 * (numberOfSquare + 1) * (numberOfSquare + 1) * 2,
+                sizeof(float) * 7 * 4, createSelectionSquare().data());
+            hasMoved = false;
+        }
         glDrawElements(
             GL_TRIANGLES,      // mode
             indices.size(),    // count
             GL_UNSIGNED_INT,   // type
-            (void*)0           // element array buffer offset
+            nullptr           // element array buffer offset
         );
 
         //buffer and drawing
@@ -116,18 +191,23 @@ unsigned Lab3Application::Run() {
 
     // Cleanup of Shader and Buffers
     shader->Unbind();
-    shader->~Shader();
     vertexArray->Unbind();
-    //vertexArray->~VertexArray();
-    unsigned int stop_error_code = stop();
-    if (stop_error_code != EXIT_SUCCESS) {
+
+    return stop();
+}
+
+unsigned Lab3Application::stop() {
+    unsigned code = GLFWApplication::stop();
+    if (GLFWApplication::stop() != EXIT_SUCCESS) {
         //print error message
         std::cerr << "Error stopping GLFW application" << std::endl;
-        std::cerr << "Error code: " << stop_error_code << std::endl;
+        std::cerr << "Error code: " << code << std::endl;
         //print opengl message
         std::cerr << "OpenGL Error: " << glGetError() << std::endl;
     }
-
-    return stop_error_code;
+    return code;
 }
 
+Lab3Application* Lab3Application::getLab3Application() {
+    return Lab3Application::current_application;
+}
--- labs/lab3/Lab3Application.h
+++ labs/lab3/Lab3Application.h
@@ -2,21 +2,36 @@
 #define PROG2002_LAB3APPLICATION_H
 
 #include <string>
+#include <vector>
 #include "GLFWApplication.h"
 
-class Lab3Application : public GLFWApplication{
-    public:
-        explicit Lab3Application(const std::string &name = "Lab2", const std::string &version = "0.0.1",
-                                 unsigned int width = 800, unsigned int height = 600);
-        ~Lab3Application();
-        unsigned Run();
+enum Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
 };
 
+class Lab3Application : public GLFWApplication {
+private:
+    unsigned int currentXSelected;
+    unsigned int currentYSelected;
+    const unsigned int numberOfSquare = 8;
+    static Lab3Application* current_application;
+    bool hasMoved;
 
-//
-// Function to implement programmed shaders: virtual function with default behaviour
-//
-GLuint CompileShader(const std::string& vertexShaderSrc, const std::string& fragmentShaderSrc);
+    std::vector<float> createSelectionSquare() const;
+public:
+    explicit Lab3Application(const std::string& name = "Lab2", const std::string& version = "0.0.1",
+        unsigned int width = 800, unsigned int height = 800);
+    ~Lab3Application();
+    unsigned Run();
+    unsigned stop();
+    void move(Direction direction);
+    static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+    static Lab3Application* getLab3Application();
+};
+
 
 
 #endif //PROG2002_LAB3APPLICATION_H
